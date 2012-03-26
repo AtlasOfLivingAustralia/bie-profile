37daa708be3ec7fd38f917c9feaec54e80917ca5
natasha.carter@csiro.au <natasha.carter@csiro.au>
2012-03-26 22:23:39 +0000

    First cut of the new "isAustralian" loader.
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-profile@3757 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ src/main/java/org/ala/dto/AustralianDTO.java
@@ -0,0 +1,78 @@
+package org.ala.dto;
+
+/**
+ * DTO for the Australian information about a species.
+ * 
+ *  
+ * @author "Natasha Carter <Natasha.Carter@csiro.au>"
+ */
+public class AustralianDTO {
+    /** Indicates that the supplied taxon guid is on the National Species List*/
+    private boolean isNSL =false;
+    /** Indicates that there are occurrence records for taxonGuid */
+    private boolean hasOccurrences=false;
+    /** Indicates that the only occurrence records found were source from CS */
+    private boolean hasCSOnly=false;
+    /** The taxonGuid that the information is about */
+    private String taxonGuid;
+    /**
+     * @return the isNSL
+     */
+    public boolean getIsNSL() {
+        return isNSL;
+    }
+    /**
+     * @param isNSL the isNSL to set
+     */
+    public void setIsNSL(boolean isNSL) {
+        this.isNSL = isNSL;
+    }
+    /**
+     * @return the hasOccurrenceRecords
+     */
+    public boolean isHasOccurrences() {
+        return hasOccurrences;
+    }
+    /**
+     * @param hasOccurrenceRecords the hasOccurrenceRecords to set
+     */
+    public void setHasOccurrenceRecords(boolean hasOccurrences) {
+        this.hasOccurrences = hasOccurrences;
+    }
+    
+    /**
+     * @return the taxonGuid
+     */
+    public String getTaxonGuid() {
+        return taxonGuid;
+    }
+    /**
+     * @param taxonGuid the taxonGuid to set
+     */
+    public void setTaxonGuid(String taxonGuid) {
+        this.taxonGuid = taxonGuid;
+    }
+    
+    
+    
+    /**
+     * @return the hasCSOnly
+     */
+    public boolean isHasCSOnly() {
+        return hasCSOnly;
+    }
+    /**
+     * @param hasCSOnly the hasCSOnly to set
+     */
+    public void setHasCSOnly(boolean hasCSOnly) {
+        this.hasCSOnly = hasCSOnly;
+    }
+    /**
+     * 
+     * @return True when the species is considered Australian.
+     */
+    public boolean getIsAustralian(){
+        return isNSL || hasOccurrences;
+    }    
+    
+}
--- src/main/java/org/ala/hbase/AustralianTaxonLoader.java
+++ src/main/java/org/ala/hbase/AustralianTaxonLoader.java
@@ -3,9 +3,18 @@ package org.ala.hbase;
 import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.regex.Pattern;
+
+import org.ala.dto.AustralianDTO;
+import org.ala.client.util.RestfulClient;
+import org.ala.dao.Scanner;
+import org.ala.dao.StoreHelper;
 import org.ala.dao.TaxonConceptDao;
 import org.ala.util.SpringUtils;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.DeserializationConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +25,8 @@ import org.springframework.stereotype.Component;
  * - concepts sourced from AFD, APC or APNI
  * - concepts that have occurrence records from geographic regions in Australia
  *
- * The list of taxon concepts are currently generated from the portal database
- * by running the is_australian_export.sql script.
- *
- * In the future this may be changed to use Cassandra and the SOLR index...
+ * The "isAustralian" status is determined from biocache-service.  In the future
+ * we may need to store the AustralianDTO object in Cassandra...
  *
  * @author Natasha Carter (Natasha.Carter@csiro.au)
  */
@@ -30,8 +37,19 @@ public class AustralianTaxonLoader {
 
     @Inject
     protected TaxonConceptDao taxonConceptDao;
+    
+    @Inject
+    protected StoreHelper storeHelper;
+    
+    protected Pattern austLsidPattern = Pattern.compile("urn:lsid:biodiversity.org.au[a-zA-Z0-9\\.:-]*");
+    
+    public static final String WS_URL = "http://biocache.ala.org.au/ws/australian/taxon/";
+
 
     public static final String AUST_GUID_FILE = "/data/bie-staging/biocache/austTaxonConcepts.txt";
+  //create restful client with no connection timeout.
+    protected RestfulClient restfulClient = new RestfulClient(0);
+
 
     public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringUtils.getContext();
@@ -41,14 +59,70 @@ public class AustralianTaxonLoader {
     }
     
     public void load() throws Exception {
-        logger.info("Starting to mark up the Australian Concepts...");
-        BufferedReader reader = new BufferedReader(new FileReader(AUST_GUID_FILE));
-        String guid = null;
-        while ((guid = reader.readLine()) != null) {
-        	String taxonGuid = taxonConceptDao.getPreferredGuid(guid);
-            taxonConceptDao.setIsAustralian(taxonGuid);
-        }
-        logger.info("Finished Australian Concept markup");
+        Scanner scanner = storeHelper.getScanner("bie", "tc", "", "taxonConcept", "hasGeoReferencedRecords");
+        byte[] guidAsBytes = null;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.getDeserializationConfig().set(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false); 
+        int total = 0;
+        int austCount=0;
+//      Map<String, String> uidInfosourceIDMap = getUidInfosourceIdMap();
+
+      while ((guidAsBytes = scanner.getNextGuid()) != null) {
+          //if the guid is biodiversity.org set true otherwise query the biocache ws
+          boolean isAust = false;
+          String guid = new String(guidAsBytes);   
+          if(scanner.getCurrentValues().containsKey("taxonConcept")){
+              
+              if(austLsidPattern.matcher(guid).matches())
+                  isAust = true;
+              else{
+                  if("true".equals(scanner.getCurrentValues().get("hasGeoReferencedRecords"))){
+                      Object[] resp = restfulClient.restGet(WS_URL+guid);
+                      if((Integer)resp[0] == HttpStatus.SC_OK){
+                          String content = resp[1].toString();
+                          if(content != null && content.length()>3){
+                              AustralianDTO adto = mapper.readValue(content, AustralianDTO.class);
+                              if(adto != null)
+                                  isAust = adto.getIsAustralian();
+                          }
+                      }
+                  }
+              }
+//              else{
+//                  Object[] resp = restfulClient.restGet(WS_URL+guid);
+//                  if((Integer)resp[0] == HttpStatus.SC_OK){
+//                      String content = resp[1].toString();
+//                      if(content != null && content.length()>3){
+//                          AustralianDTO adto = mapper.readValue(content, AustralianDTO.class);
+//                          if(adto != null)
+//                              isAust = adto.getIsAustralian();
+//                      }
+//                  }
+//    
+//              }
+          }
+          total++;
+          if(total %10000 == 0)
+              System.out.println("Processed " + total + " last id " + guid);
+          if(isAust){
+              //load the value in cassandra and then reindex the concept
+              taxonConceptDao.setIsAustralian(guid, true, true);
+              austCount++;
+          }
+              
+      }
+      
+      
+      
+      System.out.println("Total " + total + " australian: " + austCount);
+//        logger.info("Starting to mark up the Australian Concepts...");
+//        BufferedReader reader = new BufferedReader(new FileReader(AUST_GUID_FILE));
+//        String guid = null;
+//        while ((guid = reader.readLine()) != null) {
+//        	String taxonGuid = taxonConceptDao.getPreferredGuid(guid);
+//            taxonConceptDao.setIsAustralian(taxonGuid);
+//        }
+//        logger.info("Finished Australian Concept markup");
     }
 
     public void setTaxonConceptDao(TaxonConceptDao taxonConceptDao) {
